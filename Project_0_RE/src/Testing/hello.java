d21117e69fa255f51ecb9f231ea2068877375c0d
Rukiye <rukiyeeziz@gmail.com>
2020-08-04 13:35:35 -0400

    added a little sum function

--- Project_0_RE/src/Testing/hello.java
+++ Project_0_RE/src/Testing/hello.java
@@ -5,6 +5,10 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloooo");
+		int a = 5;
+		int b = 11;
+		int sum = a + b;
+		System.out.println(a + " + " + b + " = " + sum);
 
 	}
 
